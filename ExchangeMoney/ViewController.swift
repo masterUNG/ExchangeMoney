50bf3deed7ec16a38d405e30912b1f22090163bd
masterUNG <phrombutr@gmail.com>
2014-08-05 15:37:23 +0700

    Initial Commit

---
+++ ExchangeMoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExchangeMoney
+//
+//  Created by masterUNG on 8/5/2557 BE.
+//  Copyright (c) 2557 masterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
